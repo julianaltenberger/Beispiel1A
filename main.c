1860d6363ca6853962c7e731b0fb679f92ef2f31
julianaltenberger <julian.altenberger@gmail.com>
2017-10-22 15:34:35 +0200

    asd

--- main.c
+++ main.c
@@ -3,14 +3,13 @@
 char *a_arg = "stdout.txt";
 int c;
 
-while ( (c = getopt(argc, argv, "o:")) != -1) {
+c = getopt(argc, argv, "o:");
     switch(c) {
         case 'o': a_arg = optarg;
             break;
         case '?': /* invalid Option */
             break;
     }
-}
 int main(int argc, char *argv[])
 {
     give_input("asdf");
