777d4dbc715652fec9839e98332b425899f7641f
julianaltenberger <julian.altenberger@gmail.com>
2017-10-23 12:11:33 +0200

    asd

--- mycompress.c
+++ mycompress.c
@@ -10,15 +10,22 @@ void mycompress(const char *outfile, const char *infile) {
     FILE *in, *out;
     char buffer[1024];
 
+
     if ((in = fopen(infile, "r")) == NULL) {
         fprintf(stderr, "fopen input failed: %s\n", strerror(errno));
         exit(EXIT_FAILURE);
+        char stdin_string[1024];   
+        printf("Please enter a string: ");
+        printf("\n");
+        fgets(stdin_string, 1024, stdin);
     } 
         
 
     if ((out = fopen(outfile, "a")) == NULL) {
         fprintf(stderr, "fopen output failed: %s\n", strerror(errno));
         exit(EXIT_FAILURE);
+        printf("\n");
+        out = stdout;
     }
     while (fgets(buffer, sizeof(buffer), in) != NULL) {
         int i;
