bb84c0a6eb49804415bf3a27649127db41a2002c
julianaltenberger <julian.altenberger@gmail.com>
2017-10-23 12:17:20 +0200

    asd

--- mycompress.c
+++ mycompress.c
@@ -14,10 +14,6 @@ void mycompress(const char *outfile, const char *infile) {
     if ((in = fopen(infile, "r")) == NULL) {
         fprintf(stderr, "fopen input failed: %s\n", strerror(errno));
         exit(EXIT_FAILURE);
-        char stdin_string[1024];   
-        printf("Please enter a string: ");
-        printf("\n");
-        fgets(stdin_string, 1024, stdin);
     } 
         
 
@@ -63,4 +59,50 @@ void mycompress(const char *outfile, const char *infile) {
     {}
     fclose(in);
     fclose(out);
+}
+
+
+void mycompress() {
+
+    FILE *in, *out;
+    char buffer[1024];
+
+    char stdin_string[1024];   
+    printf("Please enter a string: ");
+    printf("\n");
+    fgets(stdin_string, 1024, stdin);
+    in = stdin;
+    out = stdout;
+    
+
+    while (fgets(buffer, sizeof(buffer), in) != NULL) {
+        int i;
+        int k = 1;
+        for (i = 1; buffer[i] != '\0'; i++)
+            {
+            
+                if (buffer[i] == buffer[i-1])
+                    {
+                        k++;
+                    }
+                if (buffer[i] != buffer[i-1])
+                    {
+                    
+
+                        fputc(buffer[i-1],out);
+                        fprintf(out, "%d", k);
+                        k = 1;
+
+                        if (buffer[i] == '\n')
+                            {
+                                fprintf(out, "\n");
+                                fprintf(out, "%d", 1);
+                            }
+                    }
+                
+            
+
+            }
+            
+    }
 }
--- mycompress.h
+++ mycompress.h
@@ -1,1 +1,2 @@
 void mycompress(const char *outfile, const char *infile);
+void mycompress();
